ed5047bc48a6063eb2a22b40b3b57259a98a5049
Fernando-Ibarra <fi94457@gmail.com>
2024-10-04 11:58:16 -0600

    add: deployment files && rust grpc beta

--- proyecto2/rust-server/src/main.rs
+++ proyecto2/rust-server/src/main.rs
@@ -1,6 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
+use main::athlete_client::AthleteClient;
+use main::AthleteRequest;
+
+pub mod main {
+    tonic::include_proto!("main");
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, World! From Actix Web Framework and Rust"
@@ -10,4 +17,20 @@ fn index() -> &'static str {
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+}
+
+#[tokio::main]
+async fn grpc_client() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = AthleteClient::connect("http://[::1]:3000").await?;
+    let request = tonic::Request::new(AthleteRequest {
+        student: "John Doe".into(),
+        age: 25,
+        faculty: "Computer Science".into(),
+        distance: 1000,
+    });
+
+    let response = client.get_athlete(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
